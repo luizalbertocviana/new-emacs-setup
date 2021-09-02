324c35bc65313a31a1d435929762d3fe5bf6e83b
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-09-01 22:53:57 -0300

    wip

--- init.el
+++ init.el
@@ -6,38 +6,48 @@
 (defvar new-global-map
   (copy-keymap global-map))
 
+;; helper functions
+
+(defun define-keys (keymap bindings)
+  (dolist (binding bindings)
+    (define-key keymap (kbd (nth 0 binding)) (nth 1 binding))))
+
+;; keybindings
+
 ;; esc turns on the new layer of keybindings
 (define-key old-global-map (kbd "<escape>")
   (lambda () (interactive) (use-global-map new-global-map)))
-
-;; basic movement keys
-
-(define-key new-global-map (kbd "h") 'backward-char)
-(define-key new-global-map (kbd "j") 'next-line)
-(define-key new-global-map (kbd "k") 'previous-line)
-(define-key new-global-map (kbd "l") 'forward-char)
-(define-key new-global-map (kbd "L") 'move-end-of-line)
-(define-key new-global-map (kbd "H") 'move-beginning-of-line)
 
 ;; i turns off the new layer of keybindings
 (define-key new-global-map (kbd "i")
   (lambda () (interactive) (use-global-map old-global-map)))
 
-(define-key new-global-map (kbd "C-h") 'backward-sentence)
-(define-key new-global-map (kbd "C-j") 'scroll-up-command)
-(define-key new-global-map (kbd "C-k") 'scroll-down-command)
-(define-key new-global-map (kbd "C-l") 'forward-sentence)
+;; basic movement keys
 
-(define-key new-global-map (kbd "w") 'forward-word)
-(define-key new-global-map (kbd "b") 'backward-word)
+(define-keys new-global-map
+  '(("h" backward-char)
+    ("j" next-line)
+    ("k" previous-line)
+    ("l" forward-char)
+    ("L" move-end-of-line)
+    ("H" move-beginning-of-line)
+    
+    ("C-h" backward-sentence)
+    ("C-j" scroll-up-command)
+    ("C-k" scroll-down-command)
+    ("C-l" forward-sentence)
+
+    ("w" forward-word)
+    ("b" backward-word)))
 
 ;; g keymap
 
 (defvar g-keymap
   (make-sparse-keymap))
 
-(define-key g-keymap (kbd "g") 'beginning-of-buffer)
-(define-key g-keymap (kbd "G") 'end-of-buffer)
+(define-keys g-keymap
+  '(("g" beginning-of-buffer)
+    ("G" end-of-buffer)))
 
 (define-key new-global-map (kbd "g") g-keymap)
 
@@ -46,23 +56,99 @@
 (defvar d-keymap
   (make-sparse-keymap))
 
-(define-key d-keymap (kbd "w") 'kill-word)
-(define-key d-keymap (kbd "b") 'backward-kill-word)
-(define-key d-keymap (kbd "L") 'kill-line)
-(define-key d-keymap (kbd "C-l") 'kill-sentence)
-(define-key d-keymap (kbd "v") 'kill-region)
+(define-keys d-keymap
+  '(("w" kill-word)
+    ("b" backward-kill-word)
+    ("L" kill-line)
+    ("v" kill-region)
+
+    ("C-l" kill-sentence)))
 
 (define-key new-global-map (kbd "d") d-keymap)
 
 ;; editing keys
 
-(define-key new-global-map (kbd "x") 'delete-char)
-(define-key new-global-map (kbd "u") 'undo)
-(define-key new-global-map (kbd "p") 'yank)
-(define-key new-global-map (kbd "P") 'yank-pop)
+(define-keys new-global-map
+  '(("x" delete-char)
+    ("u" undo)
+    ("p" yank)
+    ("P" yank-pop)))
+
+;; file keymap
+
+(defvar file-map
+  (make-sparse-keymap))
+
+(define-keys file-map
+  '(("f" find-file)
+    ("s" save-buffer)))
+
+;; text keymap
+
+(defvar text-map
+  (make-sparse-keymap))
+
+(define-keys text-map
+  '(("f" fill-paragraph)))
+
+;; buffer keymap
+
+(defvar buffer-map
+  (make-sparse-keymap))
+
+(define-keys buffer-map
+  '(("l" list-buffers)
+    ("b" switch-to-buffer)
+    ("s" save-some-buffers)))
+
+;; windows keymap
+
+(defvar windows-map
+  (make-sparse-keymap))
+
+(define-keys windows-map
+  '(("m" delete-other-windows)
+    ("a" apropos-command)
+    ("w" other-window)
+    ("s" split-window-below)))
+
+;; help keymap
+
+(defvar help-map
+  (make-sparse-keymap))
+
+(define-keys help-map
+  '(("m" describe-mode)
+    ("k" describe-key)
+    ("v" describe-variable)
+    ("f" describe-function)))
+
+;; leader keymap
+
+(defvar leader
+  "SPC")
+
+(defvar leader-map
+  (make-sparse-keymap))
+
+(define-keys leader-map
+  `(("w" ,windows-map)
+    ("f" ,file-map)
+    ("b" ,buffer-map)
+    ("h" ,help-map)
+    ("t" ,text-map)
+    ("q" save-buffers-kill-terminal)))
+
+(define-key new-global-map (kbd leader) leader-map)
 
 ;; visual keybindings
 
 (define-key new-global-map (kbd "v") 'set-mark-command)
+
+;; aborting keybinding
+
+(define-key new-global-map (kbd "<escape>") 'keyboard-quit)
+
+;; we start with the new global map
 
 (use-global-map new-global-map)
