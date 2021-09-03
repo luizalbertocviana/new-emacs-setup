504066c401a0138fad204db91adf995f31c3a5d6
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2021-09-03 20:05:39 -0300

    sets config across multiple files

---
+++ lisp/common.el
@@ -0,0 +1,93 @@
+(use-package smart-hungry-delete
+  :bind (("<backspace>" . smart-hungry-delete-backward-char))
+  :defer nil ;; dont defer so we can add our functions to hooks 
+  :config (smart-hungry-delete-add-default-hooks))
+
+;; colorscheme setup
+(use-package doom-themes
+  :config
+    (load-theme 'doom-tomorrow-night t))
+
+;; start screen
+(use-package dashboard
+  :custom
+    (dashboard-startup-banner 'logo)
+    (dashboard-center-content t)
+    (dashboard-set-file-icons nil)
+    (dashboard-set-footer     nil)
+    (dashboard-items          '((projects . 10)))
+    (initial-buffer-choice    (lambda () (get-buffer "*dashboard*")))
+  :config
+    (dashboard-setup-startup-hook))
+
+;; colorful delimiters (useful for editing lisp languages)
+(use-package rainbow-delimiters
+  :commands (rainbow-delimiters-mode)
+  :hook
+    (emacs-lisp-mode . rainbow-delimiters-mode))
+
+;; projectile
+(use-package projectile
+  :config
+    (projectile-mode +1))
+
+;; version control hints in fringe
+(use-package diff-hl
+  :config
+    (global-diff-hl-mode))
+
+;; displays keybindings
+(use-package which-key
+  :custom
+    (which-key-max-display-columns 4)
+  :config
+    (which-key-mode))
+
+;; autocompletion
+(use-package company
+  :hook
+    (after-init . global-company-mode)
+  :custom
+    (company-idle-delay 0)
+  :config
+    ;; displays a help popup window
+    (use-package company-quickhelp
+      :config
+        (company-quickhelp-mode))
+    ;; yasnippet
+    (use-package yasnippet
+      :config
+        (use-package yasnippet-snippets)
+        (yas-global-mode 1))
+    (push '(company-capf company-yasnippet) company-backends))
+
+;; better company/ivy autocompletion
+(use-package prescient
+  :config
+    ;; ivy integration
+    (use-package ivy-prescient
+      :config
+        (ivy-prescient-mode))
+    ;; company integration
+    (use-package company-prescient
+      :config
+      (company-prescient-mode))
+    ;; remember autocompletion choices made in previous emacs sessions
+    (prescient-persist-mode))
+
+;; for some reason it is not being automatically loaded anymore. This
+;; loads it manually as it is required by magit
+(use-package project)
+
+;; magit
+(use-package magit
+  :commands (magit-status magit-dispatch))
+
+;; lsp mode
+(use-package lsp-mode
+    :custom
+      (lsp-restart 'auto-restart)
+    :hook
+      (lsp-mode . lsp-enable-which-key-integration)
+    :config
+      (use-package lsp-ui))
---
+++ lisp/keys-minor-mode.el
@@ -0,0 +1,312 @@
+;; keys minor mode
+
+(defvar keys-minor-mode-map
+  (make-sparse-keymap))
+
+(define-minor-mode keys-minor-mode
+  "A minor mode to ensure some keybindings always take precedence"
+  :init-value t
+  :lighter " keys")
+
+(add-hook 'minibuffer-setup-hook
+	  (lambda ()
+	    (keys-minor-mode 0)))
+
+(add-hook 'after-load-functions
+	  (lambda (_file)
+	    (unless (eq (caar minor-mode-map-alist)
+			'keys-minor-mode)
+	      (let ((keys (assq 'keys-minor-mode minor-mode-map-alist)))
+		(assq-delete-all 'keys-minor-mode minor-mode-map-alist)
+		(add-to-list 'minor-mode-map-alist keys)))))
+
+;; helper functions
+
+(defun define-keys (keymap bindings)
+  (dolist (binding bindings)
+    (define-key keymap (kbd (nth 0 binding)) (nth 1 binding))))
+
+;; keybindings
+
+;; esc turns on the new layer of keybindings
+(global-set-key (kbd "<escape>") 'keys-minor-mode)
+
+;; i turns off the new layer of keybindings
+(define-key keys-minor-mode-map (kbd "i") 'keys-minor-mode)
+
+;; basic movement keys
+
+(define-keys keys-minor-mode-map
+  '(("h" backward-char)
+    ("j" next-line)
+    ("k" previous-line)
+    ("J" end-of-defun)
+    ("K" beginning-of-defun)
+    ("l" forward-char)
+    ("L" move-end-of-line)
+    ("H" move-beginning-of-line)
+    
+    ("C-h" backward-sentence)
+    ("C-j" scroll-up-command)
+    ("C-k" scroll-down-command)
+    ("C-l" forward-sentence)
+
+    ("w" forward-word)
+    ("b" backward-word)))
+
+;; g keymap
+
+(defvar g-keymap
+  (make-sparse-keymap))
+
+(define-keys g-keymap
+  '(("g" beginning-of-buffer)
+    ("l" goto-line)
+    ("G" end-of-buffer)))
+
+(define-key keys-minor-mode-map (kbd "g") g-keymap)
+
+;; d keymap
+
+(defvar d-keymap
+  (make-sparse-keymap))
+
+(define-keys d-keymap
+  '(("w" kill-word)
+    ("b" backward-kill-word)
+    ("d" kill-whole-line)
+    ("f" zap-to-char)
+    ("L" kill-line)
+    ("s" just-one-space)
+    ("S" kill-sexp)
+    ("o" delete-blank-lines)
+    ("v" kill-region)
+
+    ("C-l" kill-sentence)
+    ("C-h" backward-kill-sentence)))
+
+(define-key keys-minor-mode-map (kbd "d") d-keymap)
+
+;; v keymap
+
+(defvar v-keymap
+  (make-sparse-keymap))
+
+(define-keys v-keymap
+  '(("v" set-mark-command)
+    ("b" mark-whole-buffer)
+    ("w" mark-word)
+    ("s" mark-sexp)
+    ("p" mark-paragraph)
+    ("P" mark-page)
+    ("f" mark-defun)
+    ("r" rectangle-mark-mode)
+    ("x" exchange-point-and-mark)))
+
+(define-key keys-minor-mode-map (kbd "v") v-keymap)
+
+;; r keymap
+
+(defvar r-keymap
+  (make-sparse-keymap))
+
+(define-keys r-keymap
+  '(("m" point-to-register)
+    ("g" jump-to-register)
+    ("a" append-to-register)
+    ("w" window-configuration-to-register)
+    ("f" frameset-to-register)
+    ("y" copy-to-register)
+    ("v" view-register)
+    ("p" insert-register)))
+
+(define-key keys-minor-mode-map (kbd "r") r-keymap)
+
+;; t keymap
+
+(defvar t-keymap
+  (make-sparse-keymap))
+
+(define-keys t-keymap
+  '(("t" transpose-lines)
+    ("c" transpose-chars)
+    ("w" transpose-words)
+    ("s" transpose-sexps)))
+
+(define-key keys-minor-mode-map (kbd "t") t-keymap)
+
+;; m keymap
+
+(defvar m-keymap
+  (make-sparse-keymap))
+
+(define-keys m-keymap
+  '(("m" kmacro-end-or-call-macro)
+    ("s" kmacro-start-macro-or-insert-counter)
+    ("v" apply-macro-to-region-lines)
+    ("q" kbd-macro-query)
+    ("n" kmacro-name-last-macro)
+    ("b" kmacro-bind-to-key)
+    ("i" insert-keyboard-macro)
+    ("e" kmacro-edit-macro)))
+
+(define-key keys-minor-mode-map (kbd "m") m-keymap)
+
+;; editing keys
+
+(define-keys keys-minor-mode-map
+  '(("x" delete-forward-char)
+    ("u" undo)
+    ("o" open-line)
+    ("y" kill-ring-save)
+    ("p" yank)
+    ("P" yank-pop)
+    ("." repeat)
+    ("s" isearch-forward-symbol-at-point)
+    (">" repeat-complex-command)
+
+    ("C-s" isearch-forward-regexp)
+    ("C-r" isearch-backward-regexp)
+
+    ("M-l" completion-at-point)))
+
+;; bookmark keymap
+
+(defvar bookmark-map
+  (make-sparse-keymap))
+
+(define-keys bookmark-map
+  '(("s" bookmark-set)
+    ("g" bookmark-jump)
+    ("l" list-bookmarks)
+    ("w" bookmark-save)))
+
+;; file keymap
+
+(defvar file-map
+  (make-sparse-keymap))
+
+(define-keys file-map
+  `(("f" find-file)
+    ("d" dired)
+    ("D" ediff)
+    ("r" recover-file)
+    ("s" save-buffer)
+    ("w" write-file)
+    ("b" ,bookmark-map)))
+
+;; rectangle keymap
+
+(defvar rectangle-map
+  (make-sparse-keymap))
+
+(define-keys rectangle-map
+  '(("d" kill-rectangle)
+    ("c" clear-rectangle)
+    ("s" open-rectangle)
+    ("p" yank-rectangle)
+    ("i" string-insert-rectangle)
+    ("n" rectangle-number-lines)
+    ("r" string-rectangle)
+    ("y" copy-rectangle-as-kill)))
+
+;; text keymap
+
+(defvar text-map
+  (make-sparse-keymap))
+
+(define-keys text-map
+  `(("f" fill-paragraph)
+    ("j" delete-indentation)
+    ("v" visual-line-mode)
+    ("u" upcase-dwim)
+    ("s" ispell-word)
+    ("R" query-replace-regexp)
+    ("r" ,rectangle-map)
+    ("l" downcase-dwim)
+    ("c" count-words)))
+
+;; buffer keymap
+
+(defvar buffer-map
+  (make-sparse-keymap))
+
+(define-keys buffer-map
+  '(("l" list-buffers)
+    ("b" switch-to-buffer)
+    ("c" clone-indirect-buffer)
+    ("d" kill-this-buffer)
+    ("q" kill-buffer-and-window)
+    ("m" ibuffer)
+    ("r" revert-buffer)
+    ("s" save-some-buffers)))
+
+;; windows keymap
+
+(defvar windows-map
+  (make-sparse-keymap))
+
+(define-keys windows-map
+  '(("m" delete-other-windows)
+    ("d" delete-window)
+    ("a" apropos-command)
+    ("w" other-window)
+    ("h" windmove-left)
+    ("j" windmove-down)
+    ("k" windmove-up)
+    ("l" windmove-right)
+    ("v" split-window-right)
+    ("s" split-window-below)))
+
+;; help keymap
+
+(defvar help-map
+  (make-sparse-keymap))
+
+(define-keys help-map
+  '(("m" describe-mode)
+    ("w" woman)
+    ("a" apropos)
+    ("s" info-lookup-symbol)
+    ("b" describe-bindings)
+    ("k" describe-key)
+    ("e" info-emacs-manual)
+    ("v" describe-variable)
+    ("f" describe-function)))
+
+;; program keymap
+
+(defvar program-map
+  (make-sparse-keymap))
+
+(define-keys program-map
+  '(("c" comment-line)
+    ("B" compile)
+    ("b" recompile)))
+
+;; leader keymap
+
+(defvar leader
+  "SPC")
+
+(defvar leader-map
+  (make-sparse-keymap))
+
+(define-keys leader-map
+  `(("w" ,windows-map)
+    ("f" ,file-map)
+    ("b" ,buffer-map)
+    ("h" ,help-map)
+    ("t" ,text-map)
+    ("p" ,program-map)
+    ("q" save-buffers-kill-terminal)))
+
+(define-key keys-minor-mode-map (kbd leader) leader-map)
+
+;; aborting keybinding
+
+(define-key keys-minor-mode-map (kbd "<escape>") 'keyboard-quit)
+
+;; we start with keys minor mode enabled
+
+(keys-minor-mode 1)
